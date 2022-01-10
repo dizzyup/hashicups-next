5032ad374ea383fca68a6a898a3428411bff0b82
Hector Simpson <hector@me.com>
2022-01-10 15:43:37 +0000

    Fake API

--- pages/api/get-coffee/[id].js
+++ pages/api/get-coffee/[id].js
@@ -1,14 +1,10 @@
 import axios from 'axios'
+import { coffees } from '/mock'
 
 export default async function handler(req, res) {
   const coffee_id = req.query.id
   
-  try {
-    axios.get('http://localhost:19090/coffees').then(response => {
-      const coffee = response.data.find(item => item.id == coffee_id)
-      res.status(200).json(coffee);
-    })
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message });
-  }
+  const coffee = coffees.find(item => item.id == coffee_id)
+  
+  res.status(200).json(coffee);
 }
--- pages/api/get-coffees.js
+++ pages/api/get-coffees.js
@@ -1,11 +1,7 @@
 import axios from 'axios'
 
+import { coffees } from '/mock'
+
 export default async function handler(req, res) {
-  try {
-    axios.get('http://localhost:19090/coffees').then(response => {
-      res.status(200).json(response.data);
-    })
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message });
-  }
+  res.status(200).json(coffees);
 }
